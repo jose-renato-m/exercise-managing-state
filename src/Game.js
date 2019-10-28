698dfd1d8569aef73264e4e505f46cd7de7f906f
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-28 22:10:38 +0000

    feat: Run constructor

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class Game extends Component {
-
+  constructor(props) {
+    super(props);
+    const gameNums = this.generateNums();
+    this.state = {
+      value1: gameNums[0],
+      value2: gameNums[1],
+      value3: gameNums[2],
+      proposedAnswer: gameNums[3]
+    };
+  }
 }
+
 export default Game;
