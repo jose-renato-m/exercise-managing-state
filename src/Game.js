abc197bff61bce44bb0f5418dae82222a8f0a064
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-28 23:40:42 +0000

    feat: Generate random numbers in an array

--- src/Game.js
+++ src/Game.js
@@ -11,6 +11,16 @@ class Game extends Component {
       proposedAnswer: gameNums[3]
     };
   }
+  
+  generateNums = () => {
+    const value1 = Math.floor(Math.random() * 100);
+    const value2 = Math.floor(Math.random() * 100);
+    const value3 = Math.floor(Math.random() * 100);
+    const proposedAnswer = 
+      Math.floor(Math.random() * 3) + value1 + value2 + value3;
+    return [value1, value2, value3, proposedAnswer];
+  };
+
 }
 
 export default Game;
