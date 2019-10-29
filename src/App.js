e770b55357d8db5d841a62ab58e4f1d9344bad1a
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-29 00:46:36 +0000

    feat: Add state as a Class field

--- src/App.js
+++ src/App.js
@@ -4,14 +4,11 @@ import './App.css';
 import Game from './Game.js';
 import Score from './Score.js';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
-
 class App extends Component {
+ state = {
+   numQuestions: 0,
+   numCorrect: 0
+ };
   render() {
     return (
       <div className="App">
