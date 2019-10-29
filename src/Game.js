04968ae22e8daa5893dff9612db2e805192d181a
Jose Renato Montagnana Leite Perez <jrenato78@gmail.com>
2019-10-29 20:17:04 +0000

    refactor: Change some variable nomenclatures

--- src/Game.js
+++ src/Game.js
@@ -12,12 +12,11 @@ class Game extends Component {
     };
   }
   
-  generateNums = () => {
+  makeNewQuestion = () => {
     const value1 = Math.floor(Math.random() * 100);
     const value2 = Math.floor(Math.random() * 100);
     const value3 = Math.floor(Math.random() * 100);
-    const proposedAnswer = 
-      Math.floor(Math.random() * 3) + value1 + value2 + value3;
+    const proposedAnswer = Math.floor(Math.random() * 3) + (value1 + value2 + value3);
     return [value1, value2, value3, proposedAnswer];
   };
 
